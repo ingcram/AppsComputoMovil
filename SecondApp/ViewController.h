5eadd6502e537c7e8657f2c6a81038c34141d5dd
Roberto Alvarez <ingcram@MacBook-Pro-de-Roberto.local>
2017-09-13 19:11:46 -0500

    Initial Commit

---
+++ SecondApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SecondApp
+//
+//  Created by Roberto Alvarez on 13/09/17.
+//  Copyright © 2017 Roberto Alvarez. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SecondApp/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SecondApp
+//
+//  Created by Roberto Alvarez on 13/09/17.
+//  Copyright © 2017 Roberto Alvarez. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
